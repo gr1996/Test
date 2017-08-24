d63e55d629c8607733448d542827ec30c876fa50
Jgr1996 <1561940173@qq.com>
2017-08-24 16:31:48 +0800

    glo

--- src/jiujiu.java
+++ src/jiujiu.java
@@ -1,5 +1,7 @@
 public class jiujiu {
     public static void main(String[] args) {
         System.out.println("jiujiujiu");
+        System.out.println("jiujiu");
+        System.out.println("jiu");
     }
 }
--- src/xiuxiuxiu.java
+++ src/xiuxiuxiu.java
@@ -1,5 +1,7 @@
 public class xiuxiuxiu {
     public static void main(String[] args) {
         System.out.println("xiuxiuxixu");
+        System.out.println("xiuxiux");
+        System.out.println("xiu");
     }
 }
